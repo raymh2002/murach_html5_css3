04971bebb519223e1171e107a73fcb52661fdbc8
Raymond Harvey <raymh2002@icloud.com>
2018-10-29 16:21:21 -0400

    Initial state of Murach HTML5 & CSS3

---
+++ exercises/town_hall_5/javascript/printPage.js
@@ -0,0 +1,10 @@
+"use strict";
+var $ = function(id) {
+	return document.getElementById(id);
+}
+var printPage = function() {
+    window.print(); 
+}
+window.onload = function() {
+	$("printButton").onclick = printPage;
+}
